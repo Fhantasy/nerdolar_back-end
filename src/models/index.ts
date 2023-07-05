e74e8b96f9f03019f3b9b8e2b6b866a6f2340aa6
Alan <alan_s_pereira@hotmail.com>
2023-07-05 14:29:55 -0300

    Correção na migration de post, e feita as associações das tabelas sugeitas a alterações

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,59 @@
 import { Category } from "./Category";
+import { Comment } from "./Comment";
+import { Follow } from "./Follow";
+import { Genre } from "./Genre";
+import { Like } from "./Like";
 import { MediaProduct } from "./MediaProduct";
+import { Post } from "./Post";
+import { PostImage } from "./PostImage";
+import { User } from "./User";
+import { WatchIten } from "./WatchIten";
 
 MediaProduct.belongsTo(Category);
 Category.hasMany(MediaProduct);
+
+MediaProduct.belongsToMany(Genre, {
+  through: "media_genres",
+  as: "mediaProducts",
+});
+Genre.belongsToMany(MediaProduct, {
+  through: "media_genres",
+  as: "genres",
+});
+
+Post.belongsTo(MediaProduct);
+MediaProduct.hasMany(Post);
+
+Post.belongsTo(User);
+User.hasMany(Post);
+
+PostImage.belongsTo(Post);
+Post.hasMany(PostImage);
+
+Post.belongsToMany(User, { through: Comment });
+User.belongsToMany(Post, { through: Comment });
+
+Post.hasMany(Comment, { as: "comments", foreignKey: "post_id" });
+User.hasMany(Comment, { as: "comments", foreignKey: "user_id" });
+
+Post.belongsToMany(User, { through: Like });
+User.belongsToMany(Post, { through: Like });
+
+Post.belongsToMany(User, { through: Like });
+User.belongsToMany(Post, { through: Like });
+
+User.belongsToMany(MediaProduct, { through: WatchIten });
+MediaProduct.belongsToMany(User, { through: WatchIten });
+
+User.hasMany(WatchIten, { as: "watchIten", foreignKey: "user_id" });
+
+User.belongsToMany(User, {
+  through: Follow,
+  foreignKey: "user_following_id",
+  as: "followings",
+});
+User.belongsToMany(User, {
+  through: Follow,
+  foreignKey: "user_followed_id",
+  as: "followeds",
+});
