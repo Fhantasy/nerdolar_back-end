8c298a9008600087f988a5caca57038d5b48e6f3
Alan <alan_s_pereira@hotmail.com>
2023-09-11 14:57:00 -0300

    Preparações de deploy

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,7 +2,6 @@ import { Sequelize } from "sequelize";
 import { DATABASE_URL } from "../config/enviroment";
 
 export const sequelize = new Sequelize(DATABASE_URL, {
-  ssl: true,
   define: {
     underscored: true,
   },
