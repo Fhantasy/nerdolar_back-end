f2991cd7b9be9ae013bb440989caa87ccd8b39a2
Alan <alan_s_pereira@hotmail.com>
2023-09-12 13:54:24 -0300

    Arrumado o upload no AdminJs

--- src/adminJs/resources/user.ts
+++ src/adminJs/resources/user.ts
@@ -1,5 +1,7 @@
-import uploadFileFeature from "@adminjs/upload";
-import { FeatureType, ResourceOptions } from "adminjs";
+import uploadFileFeature, { BaseProvider } from "@adminjs/upload";
+import { FeatureType, ResourceOptions, UploadedFile } from "adminjs";
+import fs, { existsSync } from "fs";
+import { move } from "fs-extra";
 import path from "path";
 
 export const userResourceOptions: ResourceOptions = {
@@ -57,14 +59,47 @@ export const userResourceOptions: ResourceOptions = {
   ],
 };
 
+class UploadProvider extends BaseProvider {
+  constructor() {
+    super(path.join(__dirname, "../../../"));
+    if (!existsSync(path.join(__dirname, "../../../"))) {
+      throw new Error(
+        `directory: "${path.join(
+          __dirname,
+          "../../../"
+        )}" does not exists. Create it before running LocalAdapter`
+      );
+    }
+  }
+
+  // * Fixed this method because original does rename instead of move and it doesn't work with docker volume
+  public async upload(file: UploadedFile, key: string): Promise<any> {
+    const filePath =
+      process.platform === "win32" ? this.path(key) : this.path(key).slice(1); // adjusting file path according to OS
+    await fs.promises.mkdir(path.dirname(filePath), { recursive: true });
+    await move(file.path, filePath, { overwrite: true });
+  }
+
+  public async delete(key: string, bucket: string): Promise<any> {
+    await fs.promises.unlink(
+      process.platform === "win32"
+        ? this.path(key, bucket)
+        : this.path(key, bucket).slice(1)
+    ); // adjusting file path according to OS
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  public path(key: string, bucket?: string): string {
+    // Windows doesn't requires the '/' in path, while UNIX system does
+    return process.platform === "win32"
+      ? `${path.join(bucket || this.bucket, key)}`
+      : `/${path.join(bucket || this.bucket, key)}`;
+  }
+}
+
 export const userResourceFeatures: FeatureType[] = [
   uploadFileFeature({
-    provider: {
-      local: {
-        opts: {},
-        bucket: path.join(__dirname, "../../.."),
-      },
-    },
+    provider: new UploadProvider(),
     properties: {
       key: "profileImg",
       file: "profileImage",
